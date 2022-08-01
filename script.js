308a26bcb908be46527c83ac20e4e1f23a4c88cc
Bruno Henrique <bruhensant@gmail.com>
2022-08-01 20:37:17 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 console.log('rodou!')
 
-var myPix = new Array("images/lion.jpg", "images/tiger.jpg", "images/bear.jpg");
+
+function emoji() {
+
+	let emojiContainer = document.getElementById("emoji");
+
+	emojiContainer.style.opacity = 1;
+
+	const emojis = [
+		"./src/emoji/emoji-alien.png",
+		"./src/emoji/emoji-bat.png",
+		"./src/emoji/emoji-default.png",
+		"./src/emoji/emoji-lhama.png",
+		"./src/emoji/emoji-monkey.png",
+		"./src/emoji/emoji-moon.png",
+		"./src/emoji/emoji-octo.png",
+		"./src/emoji/emoji-poop.png",
+		"./src/emoji/emoji-robot.png",
+		"./src/emoji/emoji-snowperson.png",
+		"./src/emoji/emoji-sun.png",
+		"./src/emoji/emoji-thinking.png",
+	];
+
+	let randomEmoji = Math.floor(Math.random() * emojis.length);
+
+	emojiContainer.src = emojis[randomEmoji];
+	emojiContainer.style.marginBottom = '-3%';
+}
+
