f77abf59766a970b4f069eb73f95101cd4ae2058
Bruno Oliveira <bruhensant@gmail.com>
2025-12-18 21:16:09 -0300

    fix(ssr): change render mode for projects

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,15 @@
-import { RenderMode, type ServerRoute } from "@angular/ssr";
+import { RenderMode, type ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
 	{
-		path: "**",
+		path: '**',
+		renderMode: RenderMode.Server,
+	},
+	{
+		path: 'projects/:projectId',
 		renderMode: RenderMode.Prerender,
+		getPrerenderParams: async () => {
+			return [{ projectId: '1' }, { projectId: '2' }, { projectId: '3' }, { projectId: '4' }];
+		},
 	},
 ];
